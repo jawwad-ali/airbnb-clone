bfb6ece1b0d511dc59c669eb2e470913f6435b5a
jawwad-ali <alijawwad001@gmail.com>
2021-03-19 22:12:21 +0500

    date picker banner cards added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Header from "./Components/Header"
 import Home from "./Components/Home"
+import Footer from "./Components/Footer"
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
+      <Footer />
     </>
   );
 }
 
 export default App;
+// start from 2.01.52
